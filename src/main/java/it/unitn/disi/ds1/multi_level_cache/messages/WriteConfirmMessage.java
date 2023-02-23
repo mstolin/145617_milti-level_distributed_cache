2d80cb2be5d1bb86077b08f505b618c60a0f113f
Marcel Stolin <kuri-dv@mailbox.org>
2023-02-23 11:26:32 +0100

    Added key, value, and write message uuid to write confirm message class

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/WriteConfirmMessage.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/WriteConfirmMessage.java
@@ -1,16 +1,29 @@
 package it.unitn.disi.ds1.multi_level_cache.messages;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class WriteConfirmMessage implements Serializable {
 
-    private final WriteMessage writeMessage;
+    private final UUID writeMessageUUID;
+    private final int key;
+    private final int value;
 
-    public WriteConfirmMessage(WriteMessage writeMessage) {
-        this.writeMessage = writeMessage;
+    public WriteConfirmMessage(UUID writeMessageUUID, int key, int value) {
+        this.writeMessageUUID = writeMessageUUID;
+        this.key = key;
+        this.value = value;
     }
 
-    public WriteMessage getWriteMessage() {
-        return writeMessage;
+    public UUID getWriteMessageUUID() {
+        return writeMessageUUID;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
