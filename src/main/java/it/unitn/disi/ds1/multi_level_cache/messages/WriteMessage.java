fa113e4809a1db490491df10b734914b4f7ed68d
Marcel Stolin <kuri-dv@mailbox.org>
2023-02-23 11:25:59 +0100

    Added uuid to write message

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/WriteMessage.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/WriteMessage.java
@@ -1,13 +1,16 @@
 package it.unitn.disi.ds1.multi_level_cache.messages;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 public class WriteMessage implements Serializable {
 
+    private final UUID uuid;
     private final int key;
     private final int value;
 
     public WriteMessage(int key, int value) {
+        this.uuid = UUID.randomUUID();
         this.key = key;
         this.value = value;
     }
@@ -20,4 +23,7 @@ public class WriteMessage implements Serializable {
         return value;
     }
 
+    public UUID getUuid() {
+        return uuid;
+    }
 }
