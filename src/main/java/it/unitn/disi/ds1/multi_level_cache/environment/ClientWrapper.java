92e265588985b90283745eb71e25416398475761
Marcel Stolin <kuri-dv@mailbox.org>
2023-02-26 18:19:42 +0100

    Created ClientWrapper

---
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/environment/ClientWrapper.java
@@ -0,0 +1,69 @@
+package it.unitn.disi.ds1.multi_level_cache.environment;
+
+import akka.actor.ActorRef;
+import it.unitn.disi.ds1.multi_level_cache.messages.ReadMessage;
+import it.unitn.disi.ds1.multi_level_cache.messages.WriteMessage;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Random;
+
+public class ClientWrapper {
+
+    private final ActorRef actor;
+    private final List<ActorRef> l2Caches;
+
+    public ClientWrapper(ActorRef actor, List<ActorRef> l2Caches) {
+        this.actor = actor;
+        this.l2Caches = l2Caches;
+    }
+
+    private ActorRef getRandomL2Cache() {
+        Random rand = new Random();
+        return this.l2Caches.get(rand.nextInt(this.l2Caches.size()));
+    }
+
+    private Optional<ActorRef> getL2Cache(int index) {
+        try {
+            return Optional.of(this.l2Caches.get(index));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    private void sendReadMessage(int key, ActorRef l2Cache) {
+        ReadMessage message = new ReadMessage(key);
+        this.actor.tell(message, l2Cache);
+    }
+
+    private void sendWriteMessage(int key, int value, ActorRef l2Cache) {
+        WriteMessage message = new WriteMessage(key, value);
+        this.actor.tell(message, l2Cache);
+    }
+
+    public void sendReadMessage(int key, int l2Index) throws NoSuchElementException {
+        Optional<ActorRef> l2Cache = this.getL2Cache(l2Index);
+        if (l2Cache.isPresent()) {
+            this.sendReadMessage(key, l2Cache.orElseThrow());
+        }
+    }
+
+    public void sendReadMessage(int key) {
+        ActorRef l2Cache = this.getRandomL2Cache();
+        this.sendReadMessage(key, l2Cache);
+    }
+
+    public void sendWriteMessage(int key, int value, int l2Index) throws NoSuchElementException {
+        Optional<ActorRef> l2Cache = this.getL2Cache(l2Index);
+        if (l2Cache.isPresent()) {
+            this.sendWriteMessage(key, value, l2Cache.orElseThrow());
+        }
+    }
+
+    public void sendWriteMessage(int key, int value) {
+        ActorRef l2Cache = this.getRandomL2Cache();
+        this.sendWriteMessage(key, value, l2Cache);
+    }
+
+}
