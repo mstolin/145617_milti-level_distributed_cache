1918445d999bba5b60f7d7f576ef4458f20f8c0a
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-01 10:34:35 +0100

    Made client ot use DataStore as well

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/Client.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/Client.java
@@ -3,18 +3,16 @@ package it.unitn.disi.ds1.multi_level_cache.actors;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import it.unitn.disi.ds1.multi_level_cache.actors.utils.DataStore;
 import it.unitn.disi.ds1.multi_level_cache.messages.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Client extends AbstractActor {
 
     /** List of level 2 caches, the client knows about */
     private List<ActorRef> l2Caches;
-    private List<WriteMessage> writeHistory = new ArrayList<>();
-    private List<WriteMessage> readHistory = new ArrayList<>();
+    private DataStore data = new DataStore();
     final String id;
 
     public Client(int id) {
@@ -25,35 +23,42 @@ public class Client extends AbstractActor {
         return Props.create(Client.class, () -> new Client(id));
     }
 
-    private ActorRef getRandomL2Cache() {
-        Random rand = new Random();
-        ActorRef l2Cache = this.l2Caches.get(rand.nextInt(this.l2Caches.size()));
-        return l2Cache;
-    }
-
     private void onJoinL2Cache(JoinGroupMessage message) {
         this.l2Caches = List.copyOf(message.getGroup());
         System.out.printf("%s joined group of %d L2 caches\n", this.id, this.l2Caches.size());
     }
 
-    private void onWriteMessage(WriteMessage message) {
-        System.out.printf("%s sends write message to random L2 cache\n", this.id);
-        ActorRef l2Cache = this.getRandomL2Cache();
-        l2Cache.tell(message, this.getSelf());
+    private void onInstantiateWriteMessage(InstantiateWriteMessage message) {
+        System.out.printf("%s sends write message to L2 cache\n", this.id);
+        WriteMessage writeMessage = new WriteMessage(message.getKey(), message.getValue());
+        message.getL2Cache().tell(writeMessage, this.getSelf());
     }
 
     private void onWriteConfirmMessage(WriteConfirmMessage message) {
-        System.out.printf("%s received write confirm (%s)\n", this.id, message.getWriteMessageUUID().toString());
+        int key = message.getKey();
+        int value = message.getValue();
+        int updateCount = message.getUpdateCount();
+        System.out.printf("%s received write confirm for key %d (new UC: %d, old UC: %d)\n",
+                this.id, key, value, updateCount);
+        // update value
+        this.data.setValueForKey(key, value, updateCount);
     }
 
-    private void onReadMessage(ReadMessage message) {
-        System.out.printf("%s send read message to random L2 Cache\n", this.id);
-        ActorRef l2Cache = this.getRandomL2Cache();
-        l2Cache.tell(message, this.getSelf());
+    private void onInstantiateReadMessage(InstantiateReadMessage message) {
+        System.out.printf("%s send read message to L2 Cache\n", this.id);
+        int key = message.getKey();
+        ReadMessage readMessage = new ReadMessage(key, this.data.getUpdateCountForKey(key).orElse(0));
+        message.getL2Cache().tell(readMessage, this.getSelf());
     }
 
     private void onReadReplyMessage(ReadReplyMessage message) {
-        System.out.printf("Received read reply {%d: %d}\n", message.getKey(), message.getValue());
+        int key = message.getKey();
+        int value = message.getValue();
+        int updateCount = message.getUpdateCount();
+        System.out.printf("%s Received read reply {%d: %d} (new UC: %d, old UC: %d)\n",
+                this.id, key, value, updateCount, this.data.getUpdateCountForKey(key).orElse(0));
+        // update value
+        this.data.setValueForKey(key, value, updateCount);
     }
 
     @Override
@@ -61,9 +66,9 @@ public class Client extends AbstractActor {
         return this
                 .receiveBuilder()
                 .match(JoinGroupMessage.class, this::onJoinL2Cache)
-                .match(WriteMessage.class, this::onWriteMessage)
+                .match(InstantiateWriteMessage.class, this::onInstantiateWriteMessage)
                 .match(WriteConfirmMessage.class, this::onWriteConfirmMessage)
-                .match(ReadMessage.class, this::onReadMessage)
+                .match(InstantiateReadMessage.class, this::onInstantiateReadMessage)
                 .match(ReadReplyMessage.class, this::onReadReplyMessage)
                 .build();
     }
