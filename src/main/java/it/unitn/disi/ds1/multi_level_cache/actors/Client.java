94f665def64464065b19d7cda7c1fa23cc947980
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-04 18:53:45 +0100

    Updated client to follow rules and create timeouts on when needed

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/Client.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/Client.java
@@ -6,18 +6,23 @@ import akka.actor.Props;
 import it.unitn.disi.ds1.multi_level_cache.actors.utils.DataStore;
 import it.unitn.disi.ds1.multi_level_cache.messages.*;
 
-import java.io.Serializable;
 import java.util.*;
 
 public class Client extends AbstractActor {
 
     /** List of level 2 caches, the client knows about */
     private List<ActorRef> l2Caches;
+    /** Data the client knows about */
     private DataStore data = new DataStore();
-    //private Map<Serializable, ActorRef> messageQueue = new HashMap<>();
-    private Serializable currentMessage;
-    private ActorRef currentMessageReceiver;
-    private Timer timer;
+    /** Is the client waiting for a write-message */
+    private boolean hasSentWriteMessage = false;
+    /** Number of read messages the client is waiting for */
+    private int currentReadCount = 0;
+    /** Timeout for the current write conversation */
+    private Timer writeTimeout;
+    /** Timeouts for all current read conversation */
+    private Map<Integer, Timer> readTimeouts = new HashMap<>();
+    /** The clients ID */
     final String id;
 
     public Client(String id) {
@@ -28,61 +33,132 @@ public class Client extends AbstractActor {
         return Props.create(Client.class, () -> new Client(id));
     }
 
-    private boolean canInstantiateNewConversation() {
-        return this.currentMessage != null || this.currentMessageReceiver != null;
+    /**
+     * Determines if this actor is allowed to instantiate a new conversation
+     * for write. The rule is, an actor either allowed to handle a single write message
+     * or multiple read messages at the same time. Therefore, no write or any read
+     * conversation is allowed.
+     *
+     * @return Boolean that state if a write conversation is allowed
+     */
+    private boolean canInstantiateNewWriteConversation() {
+        return !this.hasSentWriteMessage && this.currentReadCount <= 0;
     }
 
-    private Optional<ActorRef> getAnotherL2Cache(ActorRef currentL2Cache) {
-        for (ActorRef l2Cache: this.l2Caches) {
-            if (l2Cache != currentL2Cache) {
-                return Optional.of(l2Cache);
-            }
-        }
-        return Optional.empty();
+    /**
+     * Determines if this actor is allowed to instantiate a new conversation
+     * for read. The rule is, an actor either allowed to handle a single write message
+     * or multiple read messages at the same time. Therefore, no current write conversation
+     * is allowed.
+     *
+     * @return Boolean that state if a read conversation is allowed
+     */
+    private boolean canInstantiateNewReadConversation() {
+        return !this.hasSentWriteMessage;
     }
 
-    private void startTimeout() {
-        this.timer = new Timer();
+    /**
+     * Initiates a timeout that triggers in 10 seconds.
+     *
+     * @return Timer that triggers the timeout
+     */
+    private Timer createTimeout() {
+        Timer timer = new Timer();
         // the delay has to be higher than the delay of the caches,
-        // otherwise it will timeout when a working L2 but is also
+        // otherwise it will time out when a working L2 but is also
         // waiting for crashed L1
-        this.timer.schedule(new TimerTask() {
+        String id = this.id;
+        timer.schedule(new TimerTask() {
             @Override
             public void run() {
-                receiverTimedOut();
+                System.out.printf("%s Message has timed out\n", id);
             }
         }, 10000);
+
+        return timer;
     }
 
-    private void stopTimeout() {
-        this.timer.cancel();
-        this.timer = null;
-        this.currentMessage = null;
-        this.currentMessageReceiver = null;
-    }
-
-    private void receiverTimedOut() {
-        // just use another L2 cache
-        if (this.currentMessage != null && this.currentMessageReceiver != null) {
-            // resend message
-            System.out.printf("%s resend message to another L2\n", this.id);
-            this.currentMessageReceiver.tell(this.currentMessage, this.getSelf());
-            this.startTimeout();
-        } else {
-            // Seems like no actor is available
-            System.out.printf("%s didn't found any available L2 caches\n", this.id);
-            this.stopTimeout();
+    /**
+     * Stops the current write timeout, if it exists.
+     */
+    private void stopWriteTimeout() {
+        if (this.writeTimeout != null) {
+            this.writeTimeout.cancel();
+            this.writeTimeout = null;
         }
     }
 
+    /**
+     * Stops the read timeout for the given key, if the
+     * timeout exists.
+     *
+     * @param key Read key
+     */
+    private void stopReadTimeout(int key) {
+        if (this.readTimeouts.containsKey(key)) {
+            this.readTimeouts.get(key).cancel();
+            this.readTimeouts.remove(key);
+        }
+    }
+
+    /**
+     * Sends a WriteMessage instance to the given L2 cache.
+     * It also starts a write-timeout.
+     *
+     * @param l2Cache The choosen L2 cache actor
+     * @param key Key that has to be written
+     * @param value Value used to update the key
+     */
+    private void tellWriteMessage(ActorRef l2Cache, int key, int value) {
+        WriteMessage writeMessage = new WriteMessage(key, value);
+        l2Cache.tell(writeMessage, this.getSelf());
+        this.hasSentWriteMessage = true;
+        this.writeTimeout = this.createTimeout();
+    }
+
+    /**
+     * Sends a ReadMessage to the given L2 cache. Additionally, it increases the read count
+     * and start a timeout for the read message.
+     *
+     * @param l2Cache Target L2 cache
+     * @param key Key to be read
+     */
+    private void tellReadMessage(ActorRef l2Cache, int key) {
+        ReadMessage readMessage = new ReadMessage(key, this.data.getUpdateCountForKey(key).orElse(0));
+        l2Cache.tell(readMessage, this.getSelf());
+        // start timeout
+        if (!this.readTimeouts.containsKey(key)) {
+            this.readTimeouts.put(key, this.createTimeout());
+        }
+        // increase count
+        this.currentReadCount = this.currentReadCount + 1;
+    }
+
+    /**
+     * Event listener that is triggered when this actor receives a
+     * JoinL2CachesMessage. Then it is supposed to join a group of L2
+     * cache actor instances.
+     *
+     * @param message The received JoinL2CachesMessage
+     */
     private void onJoinL2Caches(JoinL2CachesMessage message) {
         this.l2Caches = List.copyOf(message.getL2Caches());
         System.out.printf("%s joined group of %d L2 caches\n", this.id, this.l2Caches.size());
     }
 
+    /**
+     * Listener that is triggered whenever this actor receives a InstantiateWriteMessage.
+     * Then, the actor is supposed to send a WriteMessage to the given L2 cache.
+     *
+     * @param message The received InstantiateWriteMessage
+     */
     private void onInstantiateWriteMessage(InstantiateWriteMessage message) {
-        if (this.canInstantiateNewConversation()) {
-            System.out.printf("%s can't instantiate new conversation, because it is waiting for response\n", this.id);
+        int key = message.getKey();
+        int value = message.getValue();
+
+        if (!this.canInstantiateNewWriteConversation()) {
+            System.out.printf("%s can't instantiate new write for {%d: %d}, because it is waiting for response\n",
+                    this.id, key, value);
             return;
         }
 
@@ -91,32 +167,44 @@ public class Client extends AbstractActor {
             System.out.printf("%s The given L2 Cache is unknown\n", this.id);
             return;
         }
-        System.out.printf("%s sends write message to L2 cache\n", this.id);
 
-        // todo add l2 cache to some queue
-
-        WriteMessage writeMessage = new WriteMessage(message.getKey(), message.getValue());
-        l2Cache.tell(writeMessage, this.getSelf());
-        this.currentMessage = writeMessage;
-        this.currentMessageReceiver = l2Cache;
-        this.startTimeout();
+        System.out.printf("%s sends write message {%d: %d} to L2 cache\n", this.id, key, value);
+        this.tellWriteMessage(l2Cache, key, value);
     }
 
+    /**
+     * Listener that is triggered whenever this actor receives a WriteConfirmMessage.
+     * Then, a previous WriteMessage has been sent successfully, and this actor needs
+     * to update its value and stop the write-timeout.
+     *
+     * @param message The received WriteConfirmMessage
+     */
     private void onWriteConfirmMessage(WriteConfirmMessage message) {
         int key = message.getKey();
         int value = message.getValue();
         int updateCount = message.getUpdateCount();
-        System.out.printf("%s received write confirm for key %d (new UC: %d, old UC: %d)\n",
-                this.id, key, value, updateCount);
+        System.out.printf("%s received write confirm for key %d: %d (new UC: %d, old UC: %d)\n",
+                this.id, key, value, updateCount, this.data.getUpdateCountForKey(key).orElse(0));
         // update value
         this.data.setValueForKey(key, value, updateCount);
-        // todo remove write message from "to be confirmed queue"
-        this.stopTimeout();
+        // stop timeout
+        this.stopWriteTimeout();
+        this.hasSentWriteMessage = false;
     }
 
+    /**
+     * Listener that is triggered whenever this actor receives an InstantiateReadMessage. Then,
+     * this actor is supposed to send a ReadMessage to the given L2 cache actor, start the timeout and
+     * increase the read counter.
+     *
+     * @param message The received InstantiateReadMessage.
+     */
     private void onInstantiateReadMessage(InstantiateReadMessage message) {
-        if (this.canInstantiateNewConversation()) {
-            System.out.printf("%s can't instantiate new conversation, because it is waiting for response\n", this.id);
+        int key = message.getKey();
+
+        if (!this.canInstantiateNewReadConversation()) {
+            System.out.printf("%s can't instantiate new read conversation for key %d, because it is waiting for response\n",
+                    this.id, key);
             return;
         }
 
@@ -125,17 +213,18 @@ public class Client extends AbstractActor {
             System.out.printf("%s The given L2 Cache is unknown\n", this.id);
             return;
         }
-        int key = message.getKey();
+
         System.out.printf("%s send read message to L2 Cache for key %d\n", this.id, key);
-
-        ReadMessage readMessage = new ReadMessage(key, this.data.getUpdateCountForKey(key).orElse(0));
-        this.currentMessage = readMessage;
-        this.currentMessageReceiver = l2Cache;
-
-        l2Cache.tell(readMessage, this.getSelf());
-        this.startTimeout();
+        this.tellReadMessage(l2Cache, key);
     }
 
+    /**
+     * Event listener that is triggered whenever this actor receives a ReadReplyMessage
+     * message. Then, a previous ReadMessage was sent successfully and this actor has to update
+     * the value and stop the timer.
+     *
+     * @param message The received ReadMessage
+     */
     private void onReadReplyMessage(ReadReplyMessage message) {
         int key = message.getKey();
         int value = message.getValue();
@@ -144,8 +233,9 @@ public class Client extends AbstractActor {
                 this.id, key, value, updateCount, this.data.getUpdateCountForKey(key).orElse(0));
         // update value
         this.data.setValueForKey(key, value, updateCount);
-        // todo remove read message from "to be confirmed queue"
-        this.stopTimeout();
+        // stop timeout for key
+        this.currentReadCount = this.currentReadCount - 1;
+        this.stopReadTimeout(key);
     }
 
     @Override
