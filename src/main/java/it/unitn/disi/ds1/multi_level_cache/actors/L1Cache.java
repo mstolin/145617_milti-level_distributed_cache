7e7c2ec415662999d7e7cf5e52db47de29a73615
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-05 22:20:28 +0100

    Created extra classes for L1 and L2 cache

---
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/L1Cache.java
@@ -0,0 +1,63 @@
+package it.unitn.disi.ds1.multi_level_cache.actors;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import it.unitn.disi.ds1.multi_level_cache.messages.FillMessage;
+import it.unitn.disi.ds1.multi_level_cache.messages.RefillMessage;
+import it.unitn.disi.ds1.multi_level_cache.messages.TimeoutMessage;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+public class L1Cache extends Cache {
+
+    public L1Cache(String id) {
+        super(id, false);
+    }
+
+    @Override
+    protected void onTimeout(TimeoutMessage message) {
+        /*
+        No need to implement. Can only time-out on DB and
+        DB can't crash.
+         */
+    }
+
+    static public Props props(String id) {
+        return Props.create(Cache.class, () -> new L1Cache(id));
+    }
+
+
+    @Override
+    protected void forwardMessageToNext(Serializable message) {
+        this.database.tell(message, this.getSelf());
+    }
+
+    @Override
+    protected void multicastReFillMessage(int key, int value, int updateCount, ActorRef sender) {
+        RefillMessage reFillMessage = new RefillMessage(key, value, updateCount);
+
+        if (sender != ActorRef.noSender()) {
+            this.multicast(reFillMessage, this.l2Caches);
+        }
+        for (ActorRef cache: this.l2Caches) {
+            if (cache != sender) {
+                cache.tell(reFillMessage, this.getSelf());
+            }
+        }
+    }
+
+    @Override
+    protected void responseForFillOrReadReply(int key) {
+        // todo here we need to check if l2 has crashed, then read reply directly back to client (need to add client to the msg)
+        Optional<Integer> value = this.data.getValueForKey(key);
+        Optional<Integer> updateCount = this.data.getUpdateCountForKey(key);
+
+        if (this.currentReadMessages.containsKey(key) && value.isPresent() && updateCount.isPresent()) {
+            ActorRef cache = this.currentReadMessages.get(key);
+            FillMessage fillMessage = new FillMessage(key, value.get(), updateCount.get());
+            cache.tell(fillMessage, this.getSelf());
+            this.currentReadMessages.remove(key);
+        }
+    }
+}
---
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/L2Cache.java
@@ -0,0 +1,71 @@
+package it.unitn.disi.ds1.multi_level_cache.actors;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import it.unitn.disi.ds1.multi_level_cache.messages.ReadReplyMessage;
+import it.unitn.disi.ds1.multi_level_cache.messages.TimeoutMessage;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class L2Cache extends Cache {
+
+    public L2Cache(String id) {
+        super(id, true);
+    }
+
+    static public Props props(String id) {
+        return Props.create(Cache.class, () -> new L2Cache(id));
+    }
+
+    @Override
+    protected void crash() {
+        super.crash();
+        this.data.resetData();
+        this.currentReadMessages = new HashMap<>();
+        this.currentWriteMessage = Optional.empty();
+        this.currentWriteSender = Optional.empty();
+    }
+
+    @Override
+    protected void onTimeout(TimeoutMessage message) {
+        System.out.printf("%s - has timed out, forward message directly to DB\n");
+        this.database.tell(message, this.getSelf());
+    }
+
+    @Override
+    protected void forwardMessageToNext(Serializable message) {
+        this.mainL1Cache.tell(message, this.getSelf());
+        this.setTimeout(message, this.mainL1Cache);
+    }
+
+    @Override
+    protected void multicastReFillMessage(int key, int value, int updateCount, ActorRef sender) {
+        /*
+        No need to implement. L2 cache does not send ReFill message.
+         */
+    }
+
+    /**
+     * Sends a ReadReply message to the saved sender. A ReadReply message is only send
+     * back to the client. Therefore, no need to start a timeout, since a client is
+     * not supposed to crash.
+     *
+     * @param key The key received by the ReadMessage
+     */
+    @Override
+    protected void responseForFillOrReadReply(int key) {
+        Optional<Integer> value = this.data.getValueForKey(key);
+        Optional<Integer> updateCount = this.data.getUpdateCountForKey(key);
+
+        if (this.currentReadMessages.containsKey(key) && value.isPresent() && updateCount.isPresent()) {
+            // get client
+            ActorRef client = this.currentReadMessages.get(key);
+            this.currentReadMessages.remove(key);
+            // send message
+            ReadReplyMessage readReplyMessage = new ReadReplyMessage(key, value.get(), updateCount.get());
+            client.tell(readReplyMessage, this.getSelf());
+        }
+    }
+}
