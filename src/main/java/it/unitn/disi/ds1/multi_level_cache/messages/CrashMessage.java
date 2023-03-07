39ea94414ef3aa8eb843e34dfe065d6fc8eef476
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-07 09:35:03 +0100

    Added recover delay to CrashMessage

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/CrashMessage.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/CrashMessage.java
@@ -3,4 +3,15 @@ package it.unitn.disi.ds1.multi_level_cache.messages;
 import java.io.Serializable;
 
 public class CrashMessage implements Serializable {
+
+    private final long recoverAfterSeconds;
+
+    public CrashMessage(long recoverAfterSeconds) {
+        this.recoverAfterSeconds = recoverAfterSeconds;
+    }
+
+    public long getRecoverAfterSeconds() {
+        return recoverAfterSeconds;
+    }
+
 }
