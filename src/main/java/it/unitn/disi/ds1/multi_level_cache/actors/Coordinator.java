2f8d462216bc56a1d654bc5c12f5db48f210b413
Marcel Stolin <kuri-dv@mailbox.org>
2023-03-17 12:11:44 +0100

    Created a Coordinator interface

---
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/actors/Coordinator.java
@@ -0,0 +1,19 @@
+package it.unitn.disi.ds1.multi_level_cache.actors;
+
+public interface Coordinator {
+
+    /**
+     * Determines if all participants of the round have voted.
+     *
+     * @return Boolean stating that all participants have voted or not
+     */
+    boolean haveAllParticipantsVoted(int voteCount);
+
+    /**
+     * Is getting called when all participants voted ok.
+     */
+    void onVoteOk(int key, int value);
+
+    void abortCritWrite(int key);
+
+}
