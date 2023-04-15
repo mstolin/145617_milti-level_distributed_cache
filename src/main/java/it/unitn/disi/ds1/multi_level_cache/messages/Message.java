7d53b089a4257dd98fc90a2f5dcd4d0a96261bc4
Marcel Stolin <kuri-dv@mailbox.org>
2023-04-15 18:51:31 +0200

    Added meore public getter

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/Message.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/Message.java
@@ -13,16 +13,40 @@ public abstract class Message implements Serializable {
         this.messageConfig = messageConfig;
     }
 
-    public CacheCrashConfig getL1CrashConfig() {
-        return this.messageConfig.getL1CrashConfig();
+    public MessageConfig getMessageConfig() {
+        return this.messageConfig;
     }
 
-    public CacheCrashConfig getL2CrashConfig() {
-        return this.messageConfig.getL2CrashConfig();
+    public boolean isMessageDelayedAtL1() {
+        return this.messageConfig.isMessageDelayedAtL1();
     }
 
-    public long getDelay() {
-        return this.messageConfig.getDelay();
+    public boolean isMessageDelayedAtL2() {
+        return this.messageConfig.isMessageDelayedAtL2();
+    }
+
+    public long getL1MessageDelay() {
+        return this.messageConfig.getL1MessageDelay();
+    }
+
+    public long getL2MessageDelay() {
+        return this.messageConfig.getL2MessageDelay();
+    }
+
+    public boolean mustL1Crash() {
+        return this.messageConfig.mustL1Crash();
+    }
+
+    public boolean mustL2Crash() {
+        return this.messageConfig.mustL2Crash();
+    }
+
+    public long getL1RecoverDelay() {
+        return this.messageConfig.getL1RecoverDelay();
+    }
+
+    public long getL2RecoverDelay() {
+        return this.messageConfig.getL2RecoverDelay();
     }
 
 }
