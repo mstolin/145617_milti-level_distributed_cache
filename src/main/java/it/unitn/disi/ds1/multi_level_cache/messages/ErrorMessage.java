5aa24fabe46f83faa58df07dff9eccadae64c911
Marcel Stolin <kuri-dv@mailbox.org>
2023-04-29 11:52:19 +0200

    Added message ro error

--- src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/ErrorMessage.java
+++ src/main/java/it/unitn/disi/ds1/multi_level_cache/messages/ErrorMessage.java
@@ -10,23 +10,25 @@ public class ErrorMessage implements Serializable {
     private final ErrorType errorType;
     private final int key;
     private final MessageType messageType;
+    private final String errorMessage;
 
-    public ErrorMessage(ErrorType errorType, int key, MessageType messageType) {
+    public ErrorMessage(ErrorType errorType, int key, MessageType messageType, String errorMessage) {
         this.errorType = errorType;
         this.key = key;
         this.messageType = messageType;
+        this.errorMessage = errorMessage;
     }
 
-    public static ErrorMessage unknownKey(int key, MessageType messageType) {
-        return new ErrorMessage(ErrorType.UNKNOWN_KEY, key, messageType);
+    public static ErrorMessage unknownKey(int key, MessageType messageType, String errorMessage) {
+        return new ErrorMessage(ErrorType.UNKNOWN_KEY, key, messageType, errorMessage);
     }
 
-    public static ErrorMessage lockedKey(int key, MessageType messageType) {
-        return new ErrorMessage(ErrorType.LOCKED_KEY, key, messageType);
+    public static ErrorMessage lockedKey(int key, MessageType messageType, String errorMessage) {
+        return new ErrorMessage(ErrorType.LOCKED_KEY, key, messageType, errorMessage);
     }
 
-    public static ErrorMessage internalError(int key, MessageType messageType) {
-        return new ErrorMessage(ErrorType.INTERNAL_ERROR, key, messageType);
+    public static ErrorMessage internalError(int key, MessageType messageType, String errorMessage) {
+        return new ErrorMessage(ErrorType.INTERNAL_ERROR, key, messageType, errorMessage);
     }
 
     public ErrorType getErrorType() {
@@ -40,4 +42,9 @@ public class ErrorMessage implements Serializable {
     public MessageType getMessageType() {
         return this.messageType;
     }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+
 }
